ac2e8d0a47f45a64e6d9de969558892b29d8bfe7
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-12-17 10:16:39 +0530

    Quit app in cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QApplication>
 #include <QDeclarativeView>
+#include <QDeclarativeEngine>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     QDeclarativeView view;
+    QApplication::connect(const_cast<QDeclarativeEngine*>(view.engine()), SIGNAL(quit()), &app, SLOT(quit()));
     view.setSource(QUrl::fromLocalFile("solarwolf.qml"));
     view.show();
 
